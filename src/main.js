7d420f9def6aaf1c7a234c7d430c4224ad655969
pazi-fisch <andreas.farella@gmail.com>
2025-11-10 15:21:54 +0100

    rev 3.: added button to add a new note

--- src/main.js
+++ src/main.js
@@ -24,10 +24,49 @@ export function setupCounter(element) {
 setupCounter(document.getElementById('counter-value'));
 
 
-
+/**
+ * Configure the logic and behaviour of the page.
+ * Sets the listeners for all buttons and loads all notes from the server.
+ */
 export function setup() {
 
+    // API path
     const api_path = "http://localhost:8000/api/notes";
+
+    // dialog which is used to add/edit notes, 'mode' defines dialog behaviour
+    let mode = ''
+    const noteDialog = document.getElementById('noteDialog');
+    const noteIdLabelDialog = document.getElementById('noteIdLabelDialog');
+    const noteIdDialog = document.getElementById('noteIdDialog');
+    const noteTimestampLabelDialog = document.getElementById('noteTimestampLabelDialog');
+    const noteTimestampDialog = document.getElementById('noteTimestampDialog');
+    const noteTitleDialog = document.getElementById('noteTitleDialog');
+    const noteContentDialog = document.getElementById('noteContentDialog');
+    // set listener to perform operation when dialog is closed/confirmed
+    noteDialog.addEventListener('close', () => {
+        if (noteDialog.returnValue === 'ok') {
+            if (mode === 'add') {
+                // create new note to add
+                const newNote = {
+                    title: noteTitleDialog.value,
+                    content: noteContentDialog.value,
+                };
+                // perform POST request
+                fetch(api_path, {
+                    method: 'POST',
+                    body: JSON.stringify(newNote)
+                })
+                    .then(response => {
+                        // log response
+                        console.log(response.status + ": " + response.statusText)
+                    });
+            } else if (mode === 'edit') {
+
+            }
+            // done, refresh list
+            getAllNotes();
+        }
+    });
 
     /**
      * Get all notes from the server and display them in a list.
@@ -57,7 +96,21 @@ export function setup() {
             });
     }
 
+    function addNewNote() {
+        mode = 'add';
+        // show empty dialog to add new note, without ID and timestamp
+        noteIdLabelDialog.style.display = 'none';
+        noteTimestampLabelDialog.style.display = 'none';
+        noteTitleDialog.value = '';
+        noteContentDialog.value = '';
+        noteDialog.showModal();
+    }
+
+    // set listener for all buttons
     document.getElementById('reloadFromServer').addEventListener('click', () => getAllNotes());
+    document.getElementById('addNewNote').addEventListener('click', () => addNewNote());
+    // done setting up, default load and show all notes
+    getAllNotes();
 }
 
 setup();
